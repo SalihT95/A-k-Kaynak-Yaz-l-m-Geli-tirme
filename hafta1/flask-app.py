8d69b24da724eb991f9bccaef24de3edfd01e271
Salih Türkoğlu <turkoglusalih00@gmail.com>
2023-12-12 20:53:00 +0000

    hafta2 api projesi

--- hafta1/flask-app.py
+++ hafta1/flask-app.py
@@ -9,9 +9,9 @@ class Users(Resource):
     def get(self):
         data = pd.read_csv('users.csv')
         data = data.to_dict('records')
-        return {'data' : data}, 200
+return {'data' : data}, 200
 
-    def post(self):
+def post(self):
         json = request.get_json()
         req_data = pd.DataFrame({
             'name'      : [json['name']],
@@ -21,7 +21,7 @@ class Users(Resource):
         data = pd.read_csv('users.csv')
         data = pd.concat([data, req_data], ignore_index=True)
         data.to_csv('users.csv', index=False)
-        return {'message' : 'Record successfully added.'}, 200
+        return {'message' : 'Record successfully added.'}, 201
 
     def delete(self):
         name = request.args['name']
@@ -57,5 +57,5 @@ api.add_resource(Name, '/<string:name>')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=6768)
     app.run()
